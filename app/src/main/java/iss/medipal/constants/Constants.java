49e3ea8a690ae1b3d4596025d9f8dfc533264447
e0146835 <e0146836@u.nus.edu>
2017-03-08 16:52:53 +0800

    Commiting changes in string.xml to merge conflicts in strings.xml

---
+++ app/src/main/java/iss/medipal/constants/Constants.java
@@ -0,0 +1,12 @@
+package iss.medipal.constants;
+
+/**
+ * Created by Naveen on 3/2/17.
+ */
+
+public interface Constants {
+
+    public static final int MYMEDICINE_VIEW_PAGE_ID=2;
+    public static final String ADD_MEDICINE_PAGE="Add Medicine";
+    public static final String ADD_REMINDER_DIALOG="Add Reminder";
+}
