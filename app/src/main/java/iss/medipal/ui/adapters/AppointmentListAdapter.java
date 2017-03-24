0a6fae30a91619390d5a0155ac54997dc4390413
sreekumar <sreekumar456@gmail.com>
2017-03-24 23:23:38 +0800

    Sree:Adding list icons :Appointments

--- app/src/main/java/iss/medipal/ui/adapters/AppointmentListAdapter.java
+++ app/src/main/java/iss/medipal/ui/adapters/AppointmentListAdapter.java
@@ -5,10 +5,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import iss.medipal.R;
 import iss.medipal.model.Appointment;
@@ -21,6 +24,8 @@ public class AppointmentListAdapter extends BaseAdapter {
 
     private Context mContext;
     private List<Appointment> appointments;
+    private SimpleDateFormat dateFormatter = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+    private SimpleDateFormat timeFormatter = new SimpleDateFormat("hh:mm", Locale.getDefault());
 
     public AppointmentListAdapter(Context context, List<Appointment> appointments) {
 
@@ -49,13 +54,13 @@ public class AppointmentListAdapter extends BaseAdapter {
         if (convertView == null) {
             convertView = LayoutInflater.from(parent.getContext()).
                     inflate(R.layout.todo_list_item, parent, false);
-            viewHolder = new AppointmentListAdapter.ViewHolder(convertView);
+            viewHolder = new AppointmentListAdapter.ViewHolder(convertView,mContext);
             convertView.setTag(viewHolder);
         } else {
             viewHolder = (AppointmentListAdapter.ViewHolder) convertView.getTag();
         }
-        viewHolder.mItemTextview.setText(appointments.get(position).getLocation());
-
+        viewHolder.mItemTextview1.setText(appointments.get(position).getLocation());
+        viewHolder.mItemTextview2.setText(timeFormatter.format(appointments.get(position).getAppointment()));
         return convertView;
     }
 
@@ -69,10 +74,16 @@ public class AppointmentListAdapter extends BaseAdapter {
     class ViewHolder {
 
         //todo add more items to the list view
-        TextView mItemTextview;
+        TextView mItemTextview1;
+        TextView mItemTextview2;
+        ImageView imgThumb;
 
-        public ViewHolder(View view) {
-            mItemTextview = (TextView) view.findViewById(R.id.tvNote);
+        public ViewHolder(View view,Context context) {
+            mItemTextview1 = (TextView) view.findViewById(R.id.tvNote);
+            mItemTextview2= (TextView) view.findViewById(R.id.tvTime);
+            imgThumb =(ImageView) view.findViewById(R.id.img_row1);
+            imgThumb.setColorFilter(context.getResources().getColor(R.color.grey_700));
+
         }
     }
 }
