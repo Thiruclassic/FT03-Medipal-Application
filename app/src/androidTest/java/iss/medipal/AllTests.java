97ad0e31ecf6ce65afd0750c8a3d191e7966f7a9
Thirumal <e0146835@u.nus.edu>
2017-03-26 14:41:17 +0800

    Adding JUnit test cases for Medicine

---
+++ app/src/androidTest/java/iss/medipal/AllTests.java
@@ -0,0 +1,20 @@
+package iss.medipal;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Created by Thirumal on 26/3/2017.
+ */
+
+public class AllTests extends TestCase {
+
+    public static Test suite()
+    {
+        TestSuite suite=new TestSuite("Test for Package "+MedipalUnitTest.context.getPackageName());
+        suite.addTestSuite(MedicineUnitTest.class);
+
+        return suite;
+    }
+}
