05a62a7d3f0d529e6514b5df59e5a69254ac288e
sreekumar <sreekumar456@gmail.com>
2017-03-27 10:26:27 +0800

    Sree:Unit test appointment :Measurement help fixes

--- app/src/androidTest/java/iss/medipal/AllTests.java
+++ app/src/androidTest/java/iss/medipal/AllTests.java
@@ -4,6 +4,8 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import iss.medipal.model.Measurement;
+
 /**
  * Created by Thirumal on 26/3/2017.
  */
@@ -15,6 +17,8 @@ public class AllTests extends TestCase {
         TestSuite suite=new TestSuite("Test for Package "+MedipalUnitTest.context.getPackageName());
         suite.addTestSuite(MedicineUnitTest.class);
         suite.addTestSuite(ICEUnitTest.class);
+        suite.addTestSuite(AppointmentUnitTest.class);
+        suite.addTestSuite(MeasurementUnitTest.class);
         return suite;
     }
 }
--- app/src/main/java/iss/medipal/dao/AppointmentDao.java
+++ app/src/main/java/iss/medipal/dao/AppointmentDao.java
@@ -1,10 +1,8 @@
 package iss.medipal.dao;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import iss.medipal.model.Appointment;
-import iss.medipal.model.Medicine;
 
 
 /**
@@ -15,13 +13,9 @@ public interface AppointmentDao {
 
     public int addAppointment(Appointment appointment);
 
-    public int modifyAppointment(Appointment appointment);
-
     public int updateAppointment(Appointment appointment);
 
     public Boolean deleteAppointment(Appointment appointment);
-
-    public Appointment getAppointmentById(int appointmentId);
 
     public ArrayList<Appointment> getAllAppointments();
 
